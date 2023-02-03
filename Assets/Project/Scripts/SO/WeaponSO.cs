a0604feb92b07ece38d39aeec32fe127da9c5352
pantoygames <pantoygames@gmail.com>
2023-02-03 15:02:32 +0800

    fire interval

--- Assets/Project/Scripts/SO/WeaponSO.cs
+++ Assets/Project/Scripts/SO/WeaponSO.cs
@@ -8,16 +8,22 @@ public class WeaponSO : ScriptableObject {
     [SerializeField]
     private int timer;
     [SerializeField]
+    private float fireInterval;
+    [SerializeField]
     private int bulletAmount;
     [SerializeField]
     private float bulletDistance;
     [SerializeField]
     private BulletController bulletPrefab;
 
+    private bool _canFire = true;
+
     public int Timer => timer;
 
     public void CreateBullets(Transform direction) {
-        CoroutineHelper.Instance.StartCoroutine(CreateBulletsRoutine(direction));
+        if (_canFire) {
+            CoroutineHelper.Instance.StartCoroutine(CreateBulletsRoutine(direction));
+        }
     }
 
     private IEnumerator CreateBulletsRoutine(Transform direction) {
@@ -33,5 +39,17 @@ public class WeaponSO : ScriptableObject {
             bullet.GetComponent<BulletController>().Setup(direction.up);
             yield return new WaitForSeconds(bulletDistance);
         }
+
+        CoroutineHelper.Instance.StartCoroutine(FireIntervalRoutine());
+    }
+
+    private IEnumerator FireIntervalRoutine() {
+        _canFire = false;
+        yield return new WaitForSeconds(fireInterval);
+        _canFire = true;
+    }
+
+    private void OnEnable() {
+        _canFire = true;
     }
 }
