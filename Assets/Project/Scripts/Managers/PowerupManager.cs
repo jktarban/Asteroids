94bfa7a3f350c7ceabbeca25040da7102a0412db
pantoygames <pantoygames@gmail.com>
2023-02-03 20:23:48 +0800

    initial powerup

--- Assets/Project/Scripts/Managers/PowerupManager.cs
+++ Assets/Project/Scripts/Managers/PowerupManager.cs
@@ -1,18 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PowerupManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField]
+    private PowerupSO powerupSettings;
+ 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
